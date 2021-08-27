4714afb07c69644d0f35f7435030fb1a4b2493e9
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-08-27 17:07:46 +0700

    add featured to homepage

--- client/src/homepage/Home.jsx
+++ client/src/homepage/Home.jsx
@@ -1,14 +1,12 @@
 import "./home.scss";
 import Navbar from "../components/navbar/Navbar";
+import Featured from "../components/featured/Featured";
 
 const Home = () => {
     return (
         <div className="home">
             <Navbar />
-            <img
-                src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                alt=""
-            />
+            <Featured type="movie"/>
         </div>
     )
 }
