ba0dcb1a31939f9c141c3b3ecab01cf62ff60741
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-08-28 01:19:01 +0700

    add register page to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.scss";
 // import Home from "./pages/homepage/Home";
-import Watch from "./pages/watch/Watch";
+// import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
 
 const App = () => {
   return (
     <div className="App">
-      <Watch />
+      <Register />
     </div>
   );
 }
