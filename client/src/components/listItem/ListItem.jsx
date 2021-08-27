bf2b806b0b549eb468c02dd8ba9c8a7be2349923
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-08-27 18:34:49 +0700

    add hover effect

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -1,21 +1,46 @@
 import "./listItem.scss";
 import { PlayArrow, Add, ThumbUpAltOutlined, ThumbDownAltOutlined } from "@material-ui/icons";
+import { useState } from "react";
 
-const ListItem = () => {
+const ListItem = ({ index }) => {
+
+    const trailer = "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+    const [isHovered, setIsHoverd] = useState(false);
+
     return (
-        <div className="listItem">
+        <div 
+            className="listItem"
+            style={{ left: isHovered && index * 255 - 50 + index * 2.5 }}
+            onMouseEnter={() => setIsHoverd(true)} 
+            onMouseLeave={() => setIsHoverd(false)}
+        >
             <img
                 src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
                 alt=""
             />
-            <div className="itemInfo">
-                <div className="icons">
-                    <PlayArrow />
-                    <Add />
-                    <ThumbUpAltOutlined />
-                    <ThumbDownAltOutlined />
-                </div>
-            </div>
+            {isHovered && (
+                <>
+                    <video src={trailer} autoPlay={true} loop />
+                    <div className="itemInfo">
+                        <div className="icons">
+                            <PlayArrow className="icon"/>
+                            <Add className="icon"/>
+                            <ThumbUpAltOutlined className="icon"/>
+                            <ThumbDownAltOutlined className="icon"/>
+                        </div>
+                        <div className="itemInfoTop">
+                            <span>1 hour 14 minutes</span>
+                            <span className="limit">+16</span>
+                            <span>1999</span>
+                        </div>
+                        <div className="description">
+                            lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet
+                            lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet.
+                        </div>
+                        <div className="genre">Action</div>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
