c4c0e2a50f601cef581d3777bc245aa02c573837
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-04 00:44:13 +0700

    connect database using mongoose

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connection using MongoDB is successfully."))
+  .catch((err) => console.log("Error", err));
 
 app.listen(8800, () => {
   console.log("Backend server is started on port 8800.");
