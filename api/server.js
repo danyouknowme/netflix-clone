63e97115a47c0755ed97cc650a340035077acae8
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-04 12:52:19 +0700

    add dependency crypto-js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,9 @@ mongoose
   })
   .then(() => console.log("Database connection using MongoDB is successfully."))
   .catch((err) => console.log("Error", err));
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is started on port 8800.");
