79e9fcb1b100863330ef097d60caf2e8020ce0f9
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-11-19 00:53:49 +0700

    create user stats operation

--- api/routes/users.js
+++ api/routes/users.js
@@ -68,5 +68,24 @@ router.get("/", verify, async (req, res) => {
 });
 
 // Get user stats
+router.get("/stats", async (req, res) => {
+  try {
+    const data = await User.aggregate([
+      {
+        $project: {
+          month: { $month: "$createdAt" }
+        }
+      }, {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 }
+        }
+      }
+    ]);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 module.exports = router;
