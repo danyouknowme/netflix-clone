fb29948887a122a83b7599b44309eb07d5c5b401
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-11-21 22:44:42 +0700

    add get movie operation

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -48,4 +48,49 @@ router.delete("/:id", verify, async (req, res) => {
     }
 });
 
+// Get movie
+router.get("/find/:id", verify, async (req, res) => {
+    try {
+        const movie = Movie.findById(req.params.id);
+        res.status(200).json(movie);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Get random movie
+router.get("/random", verify, async (req, res) => {
+    const type = req.query.type;
+    let movie;
+    try {
+        if (type === "series") {
+            movie = await Movie.aggregate([
+                { $match: { isSeries: true } },
+                { $sample: { size: 1 } }
+            ]);
+        } else {
+            movie = await Movie.aggregate([
+                { $match: { isSeries: false } },
+                { $sample: { size: 1} }
+            ]);
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Get all movie
+router.get("/", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            const movies = await Movie.find();
+            res.status(200).json(movies);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
 
 dotenv.config();
 const app = express();
@@ -16,8 +17,10 @@ mongoose
   .catch((err) => console.log("Error", err));
 
 app.use(express.json());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movie", movieRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is started on port 8800.");
