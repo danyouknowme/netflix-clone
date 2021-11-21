c423fda52db5b04d98e89cce8c088bfe39b0aa45
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-11-21 22:25:39 +0700

    add update movie operation

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -9,7 +9,23 @@ router.post("/", verify, async (req, res) => {
 
         try {
             const savedMovie = await newMovie.save();
-            res.status(201).json(savedMovie);
+            res.status(200).json(savedMovie);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed!");
+    }
+});
+
+// Update movie
+router.put("/:id", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            }, { new: true });
+            res.status(200).json(updatedMovie);
         } catch (err) {
             res.status(500).json(err);
         }
