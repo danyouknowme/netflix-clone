fff314f18a03e7e1cb443aba7e15bf6a2fcb1e5c
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-11-23 21:48:36 +0700

    fetching data from api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Login from "./pages/login/Login";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 const App = () => {
-	const user = false;
+	const user = true;
 	return (
 		<Router>
 			<Switch>
--- client/src/pages/homepage/Home.jsx
+++ client/src/pages/homepage/Home.jsx
@@ -1,19 +1,46 @@
+import { useEffect, useState } from "react";
 import "./home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
+import axios from "axios";
 
 const Home = ({ type }) => {
-    return (
-        <div className="home">
-            <Navbar />
-            <Featured type={type} />
-            <List />
-            <List />
-            <List />
-            <List />
-        </div>
-    )
-}
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/api/lists${type ? "?type=" + type : ""}${genre ? "&genre=" + genre : ""}`, {
+            headers: {
+              token:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOTY5MWY3NTZhMWNlNzQ4OTAwNDgyNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzU5OTQxNywiZXhwIjoxNjM4MDMxNDE3fQ.e3PMUnmpwa81RvnHOKawWXaaS0j6mAzz67PxZITtPP8",
+            },
+          }
+        ).then(response => console.log(response)
+        .catch((err) => {
+            console.log(err);
+        }));
+        console.log(response);
+        // setLists(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomLists();
+  }, [type, genre]);
+  return (
+    <div className="home">
+      <Navbar />
+      <Featured type={type} />
+      <List />
+      <List />
+      <List />
+      <List />
+    </div>
+  );
+};
 
 export default Home;
