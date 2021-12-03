4de43cb3854ceb4954435ac5bbf7fac3ec134d10
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-12-03 23:20:38 +0700

    fetching data from api

--- client/src/pages/homepage/Home.jsx
+++ client/src/pages/homepage/Home.jsx
@@ -10,51 +10,30 @@ const Home = ({ type }) => {
   const [genre, setGenre] = useState(null);
 
   useEffect(() => {
+    const getRandomList = async () => {
+      try {
 
-    const options = {
-      method: 'GET',
-      url: 'https://imdb8.p.rapidapi.com/auto-complete',
-      params: {q: 'game of thr'},
-      headers: {
-        'x-rapidapi-host': 'imdb8.p.rapidapi.com',
-        'x-rapidapi-key': '28adc9baa5msha9ae4e65ee62004p11d19fjsne0a203707695'
+        const res = await axios.get(
+          `http://localhost:8800/api/lists${type ? "?type=" + type : ""}${genre ? "&genre=" + genre : ""}`, {
+            headers: {
+              token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOTY5MWY3NTZhMWNlNzQ4OTAwNDgyNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzODM1OTQwOSwiZXhwIjoxNjM4NzkxNDA5fQ.QHFKQrmw8JxJIrP8Pegh6TItpx7i5JR1j_XaE0OEFtY'
+            }
+          }
+        );
+        setLists(res.data);
+      } catch (err) {
+        console.log(err);
       }
     };
-
-    axios.request(options).then(function (response) {
-      console.log(response.data);
-    }).catch(function (error) {
-      console.error(error);
-    });
-    // const getRandomLists = async () => {
-    //   try {
-    //     const response = await axios.get(
-    //       `http://localhost:8080/api/lists${type ? "?type=" + type : ""}${genre ? "&genre=" + genre : ""}`, {
-    //         headers: {
-    //           token:
-    //             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOTY5MWY3NTZhMWNlNzQ4OTAwNDgyNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzU5OTQxNywiZXhwIjoxNjM4MDMxNDE3fQ.e3PMUnmpwa81RvnHOKawWXaaS0j6mAzz67PxZITtPP8",
-    //         },
-    //       }
-    //     ).then(response => console.log(response)
-    //     .catch((err) => {
-    //         console.log(err);
-    //     }));
-    //     console.log(response);
-    //     // setLists(response.data);
-    //   } catch (err) {
-    //     console.log(err);
-    //   }
-    // };
-    // getRandomLists();
+    getRandomList();
   }, [type, genre]);
   return (
     <div className="home">
       <Navbar />
       <Featured type={type} />
-      <List />
-      <List />
-      <List />
-      <List />
+      {lists.map((list) => (
+        <List list={list}/>
+      ))}
     </div>
   );
 };
